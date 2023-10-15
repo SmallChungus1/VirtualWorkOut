58c7a039d3f0338b17c42cf4014df51af936d12e
SmallChungus1 <hanson.l@wustl.edu>
2023-10-15 09:29:43 -0500

    finished everthing I think

--- views.py
+++ views.py
@@ -3,6 +3,9 @@ from flask import Blueprint, render_template, request, url_for
 
 
 import workouts.jumpingJacks2 as jumpingJacks2
+import workouts.pushup2 as pushUp
+import workouts.tennis as tennisF
+import workouts.tennisBH as tennisB
 #from workouts.jumpingJacks2 import finalScore
 
 views = Blueprint(__name__, "views")
@@ -12,9 +15,6 @@ views = Blueprint(__name__, "views")
 #     return render_template("index.html", name="User")
 
 
-@views.route('/test')
-def test():
-    return"hello"
 
 @views.route("/register")
 def register():
@@ -47,6 +47,71 @@ def restartjumpingJacks():
     jumpingJacks2.restart_JJ()
     return render_template("jumpingJacks.html")
 
-@views.route("/pushups")
-def pushups():
-    return "none for now"
+# @views.route("/pushups")
+# def pushups():
+#     return "none for now"
+
+@views.route("/pushUps")
+def pushUpsPage():
+    return render_template("pushUps.html")
+
+@views.route("/runPushUps")
+def runPushUps():
+    return pushUp.run_push_ups_counter()
+
+
+@views.route("/terminatePushUps", methods=['GET'])
+def terminatePushUps():
+    finalScore, caloriesBurned = pushUp.terminate_PU()
+    print("Final score: ", finalScore)
+    return render_template("pushUps.html", userScore=finalScore, caloriesCount=caloriesBurned)
+
+
+@views.route("/restartPushUps")
+def restartPushUps():
+    pushUp.restart_PU()
+    return render_template("pushUps.html")
+
+
+@views.route("/TennisForehand")
+def tennisForeHand():
+    return render_template("tennisForehand.html")
+
+@views.route("/runTF")
+def runTF():
+    return tennisF.run_tennisF_counter()
+
+
+@views.route("/terminateTF", methods=['GET'])
+def terminateTF():
+    finalScore, caloriesBurned = tennisF.terminate_TF()
+    print("Final score: ", finalScore)
+    return render_template("tennisForehand.html", userScore=finalScore, caloriesCount=caloriesBurned)
+
+
+@views.route("/restartTF")
+def restartPF():
+    tennisF.restart_TF()
+    return render_template("tennisForehand.html")
+
+@views.route("/TennisBH")
+def tennisBH():
+    return render_template("tennisBH.html")
+
+@views.route("/runTB")
+def runTB():
+    return tennisB.run_tennisBH_counter()
+
+
+@views.route("/terminateTB", methods=['GET'])
+def terminateBH():
+    finalScore, caloriesBurned = tennisB.terminate_TB()
+    print("Final score: ", finalScore)
+    return render_template("tennisBH.html", userScore=finalScore, caloriesCount=caloriesBurned)
+
+
+@views.route("/restartTB")
+def restartTB():
+    tennisB.restart_TB()
+    return render_template("tennisBH.html")
+
